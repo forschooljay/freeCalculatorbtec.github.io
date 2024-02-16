0bcbb30cf5475138373bfc01b3e883a74b9b28c2
forschooljay <149126811+forschooljay@users.noreply.github.com>
2024-02-16 08:36:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function percentage(input) {    
+    display.value /= 100;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function bracket(input) {
+    if (input === "(") {
+        display.value += "(";
+    } else if (input === ")") {
+        display.value += ")";
+    }
+}
+
+function sin(){
+    display.value= display.value * (Math.PI/180)
+    display.value= Math.sin(display.value);
+}
+
+function cos(){
+    display.value= display.value * (Math.PI/180)
+    display.value= Math.cos(display.value);
+}
+
+function tan(){
+    display.value= display.value * (Math.PI/180)
+    display.value= Math.tan(display.value);
+}
+
+function square(){
+    display.value *= display.value;
+}
+
+function pi(){
+    display=3.1415926536;
+}
+
+  
